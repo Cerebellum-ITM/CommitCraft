7468da98ba34740b86492f5e1707ba2a7a7db422
Pascual Chávez <pascual.chavez@tecmor.mx>
2025-04-20 15:04:26 -0600

    [ADD] main.py: The basic scheme of the options was added

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # CommitCraft
+
+## Configuring Commit Types
+
+This tool allows customization of commit types through configuration files, which can be placed in different directories with varying levels of priority:
+
+1. Local Configuration: Highest priority. Located in the execution directory `.commitCraftConfig.toml`.
+2. Global Configuration: Medium priority. Located in `HOME/.config/CommitCraft/config.toml`.
+3. Default Configuration: Lowest priority. Located in the tool's directory as `default_config.toml`.
+
+## Structure of the Configuration File
+
+The configuration file should be in TOML format and include a `[commit_types]` section. Each commit type should be defined as a table within a list. Here is an example structure:
+
+```toml
+[commit_types]
+
+[[commit_types.types]]
+name = "feat"
+description = "A new feature"
+color = "#00FF00"
+
+[[commit_types.types]]
+name = "fix"
+description = "A bug fix"
+color = "#FF0000"
+
+[[commit_types.types]]
+name = "docs"
+description = "Documentation only changes"
+color = "#0000FF"
+
+```
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import os
+from typing import DefaultDict, Union
+import toml
 import subprocess
 
 from textual.app import App, ComposeResult
 from textual.widgets import Footer, Header, RichLog
 
 VERSION: str = '0.1.0'
+CURRENT_DIRECTORY: str = os.getcwd()
+SCRIPT_PATH: str = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_CONFIG: str = os.path.join(SCRIPT_PATH, 'defaultConfig.toml')
+GLOBAL_CONFIG: str = os.path.join(
+    os.path.expanduser('~'), '.config', 'CommitCraft', 'config.toml'
+)
+LOCAL_CONFIG: str = os.path.join(CURRENT_DIRECTORY, '.commitCraftConfig.toml')
 
 
 class CommitCraft(App):
@@ -26,10 +35,68 @@ class CommitCraft(App):
         directory_info = self._get_current_directory_info()
         self.app_logger.write(directory_info)
         self.sub_title = f'Working dir: {directory_info["directory_name"]} - Branch: {directory_info["branch_name"]}'
+        config = self.load_config()
+        self.app_logger.write(self.commit_types)
 
     # ---------------------------------------------------------
     # HELPERS
     # ---------------------------------------------------------
+    def _extract_attribute_value(self, level: str, attribute: str) -> dict:
+        if level == 'local':
+            return self.local_config[attribute]
+        elif level == 'global':
+            return self.global_config[attribute]
+        else:
+            return self.default_config[attribute]
+
+    def _set_config_priority_level(self, attribute: str) -> list:
+        config_file_priority = []
+        if self.local_config.get(attribute, False):
+            config_file_priority.append('local')
+        if self.global_config.get(attribute, False):
+            config_file_priority.append('global')
+        config_file_priority.append('default')
+        return config_file_priority
+
+    def _load_toml_config(self, path: str = DEFAULT_CONFIG) -> dict:
+        return toml.load(path) if os.path.exists(path) else {}
+
+    def _get_config_values(self, attribute: str) -> dict:
+        values = {}
+        config_file_priority = self._set_config_priority_level(attribute=attribute)
+        self.app_logger.write(config_file_priority)
+        max_priority_level = self._extract_attribute_value(
+            level=config_file_priority[0], attribute=attribute
+        )
+        min_priority_level = (
+            self._extract_attribute_value(
+                level=config_file_priority[1], attribute=attribute
+            )
+            if len(config_file_priority) > 1
+            else False
+        )
+        if attribute == 'commit_types' and min_priority_level:
+            global_config = self.global_config[attribute]
+            mode = global_config['config'][0]['mode'] or 'replace'
+            if mode == 'Append':
+                max_priority_level['types'] = (
+                    max_priority_level['types'] + min_priority_level['types']
+                )
+            return max_priority_level
+        return values
+
+    def load_config(self) -> dict:
+        """
+        It looks for a configuration file first in the execution directory, then for a global one in '$HOME/.config/CommitCraft/', if it doesn't find it, it will use the default one.
+        """
+        config = {}
+        self.default_config: dict = self._load_toml_config()
+        self.global_config: dict = self._load_toml_config(path=GLOBAL_CONFIG)
+        self.local_config: dict = self._load_toml_config(path=LOCAL_CONFIG)
+        self.commit_types = self._get_config_values(attribute='commit_types')
+
+        return config
+
     def _run_git_command(self, opts: list[str]) -> str:
         args = ['git'] + opts
         command_result = ''
@@ -49,7 +116,7 @@ class CommitCraft(App):
         return command_result
 
     def _get_current_directory_info(self) -> dict[str, str]:
-        directory_name = os.path.basename(os.getcwd())
+        directory_name = os.path.basename(CURRENT_DIRECTORY)
         branch_name = self._run_git_command(['rev-parse', '--abbrev-ref', 'HEAD'])
         return {
             'directory_name': directory_name,
